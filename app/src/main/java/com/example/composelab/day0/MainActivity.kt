16811272021cdd1b0678e193cd43b2b452ea5cdb
achmad2002 <19kaizen@gmail.com>
2023-06-03 17:13:18 +0700

    move file to package

--- app/src/main/java/com/example/composelab/day0/MainActivity.kt
+++ app/src/main/java/com/example/composelab/day0/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.example.composelab
+package com.example.composelab.day0
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
--- app/src/main/java/com/example/composelab/day0/Screen.kt
+++ app/src/main/java/com/example/composelab/day0/Screen.kt
@@ -1,4 +1,6 @@
-package com.example.composelab
+package com.example.composelab.day0
+
+import com.example.composelab.R
 
 enum class Screen(val route: String, var titleId: Int) {
     HomePageScren("homepage_screen", R.string.homepage_screen),
